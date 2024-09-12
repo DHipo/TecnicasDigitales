6c3db834155c13b80098ba97822d868d7d710de2
Máximo Zúñiga <mjzunigaricardi@mail.austral.edu.ar>
2024-09-12 10:35:43 -0300

    ACCESS_POINT

--- include/Network.h
+++ include/Network.h
@@ -1,8 +1,22 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
 namespace Network {
+
+    const char* apSSID = "PEN-PLOTER";      // Nombre de la red WiFi para el punto de acceso
+    const char* apPassword = "123456789"; // Contraseña de la red WiFi para el punto de acceso
+
+    void setupAccessPoint(const char* ssid, const char* password) {
+    WiFi.softAP(ssid, password);
+
+    // Obtener la dirección IP del Access Point
+    IPAddress IP = WiFi.softAPIP();
+        Serial.print("Access Point IP Address: ");
+        Serial.println(IP);
+
+    }
     void Init() 
     {
-        Serial.println("Hola desde Network");
+        setupAccessPoint(apSSID, apPassword);
     }
 };
