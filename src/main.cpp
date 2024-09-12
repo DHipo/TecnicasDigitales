3af5358c7c1128f859e8feee3e317d38cecf57f9
DHipo <bautidhipolito@gmail.com>
2024-09-12 12:35:53 -0300

    gcode

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,13 @@
 #include <Arduino.h>
 #include "Engine.h"
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-
-  Engine::Init();
+  Serial.begin(115200);
+  Engine::Init(9600);
+  if (!Engine::SelectFile("cuadrado.gcode")) return;
+  Engine::StartPrintingAsync();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
